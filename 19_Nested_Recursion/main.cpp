8c28ac2c28b6e6340e3687870beb47629e7bcef3
fFukurou <supevi1@gmail.com>
2024-10-31 16:37:35 -0300

    Indirect Recursion, Nested Recursion

---
+++ 19_Nested_Recursion/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int n)
+{
+    if(n>100)
+        return n-10;
+
+    return fun(fun(n+11));
+}
+
+int main()
+{
+    int r;
+    r = fun(95);
+    printf("%d\n", r);
+
+
+
+    return 0;
+}
